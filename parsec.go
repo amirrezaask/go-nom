37c935116783b46851e8ddb8284ce3dc332a0ab2
amirrezaask <raskarpour@gmail.com>
2023-03-21 06:38:16 +0330

    initial commit

---
+++ parsec.go
@@ -0,0 +1,167 @@
+package parsec
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrEOF = errors.New("reached eof")
+
+type Scanner interface {
+	EOF() bool
+	GetChar() rune
+	Forward()
+}
+
+type Parser[OUT any] struct {
+	Name string
+	f    func(Scanner) (OUT, error)
+}
+
+func (p *Parser[OUT]) Parse(s Scanner) (OUT, error) {
+	return p.f(s)
+}
+
+type StringScanner struct {
+	s   string
+	cur int
+}
+
+func NewStringScanner(s string) *StringScanner {
+	return &StringScanner{s: s, cur: 0}
+}
+
+func (s *StringScanner) EOF() bool {
+	return !(s.cur < len(s.s))
+}
+
+func (s *StringScanner) GetChar() rune {
+	return rune(s.s[s.cur])
+}
+
+func (s *StringScanner) Forward() {
+	s.cur++
+}
+
+func Char(c rune) Parser[rune] {
+	return Parser[rune]{
+		Name: fmt.Sprintf("char parser: '%c'", c),
+		f: func(s Scanner) (rune, error) {
+			if s.EOF() {
+				return 0, ErrEOF
+			}
+			char := s.GetChar()
+			if char != c {
+				return 0, errors.New("char not matched")
+			}
+			s.Forward()
+			return char, nil
+		},
+	}
+}
+
+func Sequence[IN, OUT any](mapper func([]IN) OUT, ps ...Parser[IN]) Parser[OUT] {
+	return Parser[OUT]{
+		Name: "sequence",
+		f: func(s Scanner) (OUT, error) {
+			o := new(OUT)
+
+			var values []IN
+			for _, p := range ps {
+				value, err := p.Parse(s)
+				if err != nil {
+					return *o, err
+				}
+				values = append(values, value)
+			}
+			if mapper == nil {
+				return *o, nil
+			}
+			return mapper(values), nil
+		},
+	}
+}
+
+func OneOf[T any](ps ...Parser[T]) Parser[T] {
+	return Parser[T]{
+		Name: "oneof",
+		f: func(s Scanner) (T, error) {
+			o := new(T)
+
+			for _, p := range ps {
+				value, err := p.Parse(s)
+				if err != nil {
+					continue
+				}
+				return value, nil
+			}
+
+			return *o, errors.New("no parser matched")
+		},
+	}
+}
+
+func OneOrMore[IN, OUT any](mapper func([]IN) OUT, parser Parser[IN]) Parser[OUT] {
+	return Parser[OUT]{
+		Name: "one or more",
+		f: func(s Scanner) (OUT, error) {
+			var values []IN
+			var err error
+			value, err := parser.Parse(s)
+			if err != nil {
+				return *new(OUT), errors.New("one or more should at least matched once")
+			}
+			values = append(values, value)
+			for !s.EOF() {
+				value, err := parser.Parse(s)
+				if err != nil {
+					break
+				}
+				values = append(values, value)
+			}
+			if mapper == nil {
+				return *new(OUT), nil
+			}
+			return mapper(values), nil
+		},
+	}
+}
+
+func ZeroOrMore[IN, OUT any](mapper func([]IN) OUT, parser Parser[IN]) Parser[OUT] {
+	return Parser[OUT]{
+		Name: "zero or more",
+		f: func(s Scanner) (OUT, error) {
+			var values []IN
+			for !s.EOF() {
+				value, err := parser.Parse(s)
+				if err != nil {
+					break
+				}
+				values = append(values, value)
+			}
+			if mapper == nil {
+				return *new(OUT), nil
+			}
+			return mapper(values), nil
+		},
+	}
+}
+
+func ZeroOrOne[IN, OUT any](mapper func(*IN) OUT, parser Parser[IN]) Parser[OUT] {
+	return Parser[OUT]{
+		Name: "zero or one",
+		f: func(s Scanner) (OUT, error) {
+			value, err := parser.Parse(s)
+			if err != nil {
+				if mapper == nil {
+					return *new(OUT), nil
+				}
+				return mapper(nil), nil
+			}
+			if mapper == nil {
+				return *new(OUT), nil
+			}
+			return mapper(&value), nil
+		},
+	}
+}
---
+++ parsec_test.go
@@ -0,0 +1,75 @@
+package parsec
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChar(t *testing.T) {
+	s := NewStringScanner("c")
+	parser := Char('c')
+	c, err := parser.Parse(s)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, 'c', c)
+
+	assert.Equal(t, 1, s.cur)
+	assert.Equal(t, true, s.EOF())
+}
+
+func TestSeq(t *testing.T) {
+	s := NewStringScanner("char")
+	charKeywordParser := Sequence[rune, bool](nil, Char('c'), Char('h'), Char('a'), Char('r'))
+
+	_, err := charKeywordParser.Parse(s)
+	assert.NoError(t, err)
+}
+
+func TestOneOf(t *testing.T) {
+	s := NewStringScanner("true")
+	trueKeywordParser := Sequence(func(i []rune) bool { return true }, Char('t'), Char('r'), Char('u'), Char('e'))
+	falseKeywordParser := Sequence(func(i []rune) bool { return false }, Char('f'), Char('a'), Char('l'), Char('s'), Char('e'))
+	booleanParser := OneOf(trueKeywordParser, falseKeywordParser)
+
+	b, err := booleanParser.Parse(s)
+	assert.NoError(t, err)
+	assert.True(t, b)
+}
+
+func TestOneOrMore(t *testing.T) {
+	s := NewStringScanner("bbbb")
+	bParser := Char('b')
+	bsParser := OneOrMore(func(i []rune) int {
+		return len(i)
+	}, bParser)
+
+	b, err := bsParser.Parse(s)
+	assert.NoError(t, err)
+	assert.Equal(t, 4, b)
+}
+
+func TestZeroOrMore(t *testing.T) {
+	s := NewStringScanner("bbbb")
+	bParser := Char('b')
+	bsParser := ZeroOrMore(func(i []rune) int {
+		return len(i)
+	}, bParser)
+
+	b, err := bsParser.Parse(s)
+	assert.NoError(t, err)
+	assert.Equal(t, 4, b)
+}
+
+func TestZeroOrOne(t *testing.T) {
+	s := NewStringScanner("bbbb")
+	bParser := Char('b')
+	bsParser := ZeroOrOne(func(i *rune) bool {
+		return i != nil
+	}, bParser)
+
+	b, err := bsParser.Parse(s)
+	assert.NoError(t, err)
+	assert.True(t, b)
+}
